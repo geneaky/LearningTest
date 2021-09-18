f37115122f97592259728920515a83dc7cb58602
geneaky <kaktus418@gmail.com>
2021-09-18 22:02:58 +0900

    spring data redis learning

---
+++ springDataJpaRedis/src/main/java/com/example/springdatajparedis/SpringDataJpaRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatajparedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaRedisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringDataJpaRedisApplication.class, args);
+  }
+
+}
---
+++ springDataJpaRedis/src/test/java/com/example/springdatajparedis/SpringDataJpaRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdatajparedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaRedisApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
