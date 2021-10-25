ff71bc9b7140e3daf9e01b730d81874cbb4de7b5
geneaky <kaktus418@gmail.com>
2021-10-25 23:03:26 +0900

    permutation2

---
+++ Practice/src/main/java/Permutation/Permutation2.java
@@ -0,0 +1,35 @@
+package Permutation;
+
+public class Permutation2 {
+
+  public static void main(String[] args) {
+    Permutation2 ex = new Permutation2();
+    int[] arr = {1,2,3,4};
+
+    ex.doPermutation(arr,0);
+  }
+
+  private void doPermutation(int[] arr, int startIdx) {
+    int length = arr.length;
+    if(startIdx == length -1 ){
+      for(int n: arr){
+        System.out.print(n + " ");
+      }
+      System.out.println();
+      return;
+    }
+
+    for(int i = startIdx; i<length;i++){
+      swap(arr,startIdx,i);
+      doPermutation(arr,startIdx + 1);
+      swap(arr,startIdx,i);
+    }
+  }
+
+  private void swap(int[] arr, int n1, int n2) {
+    int temp = arr[n1];
+    arr[n1] = arr[n2];
+    arr[n2] = temp;
+  }
+
+}
